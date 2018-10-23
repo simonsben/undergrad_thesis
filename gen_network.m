ad5340f0d4afaf9892e2dd3dce798c8e40eca042
simonsben <simons.ben0@gmail.com>
2018-10-23 13:21:19 -0400

    Close maybe to getting a netowrk without disconnected nodes

--- gen_network.m
+++ gen_network.m
@@ -1,19 +1,19 @@
-clear;
+clear all;
 %% Generate graph
 n = 10;  % Number of nodes
 connections = zeros(n); % Start with no connections
 
-for i = 1:n % For each row
-   for j = i:n % For each column
+for i = 1:length(connections) % For each row
+   for j = 1:(i-1) % For each column
        connection_sum = sum(connections(:)) + 1; % Sum matrix
        row_sum = sum(connections(:,j)) + 1; % Sum row
        
-       if i == n % Don't do self connections
+       if i == j % Don't do self connections
           continue; 
        elseif evaluate_probability(row_sum / connection_sum)
            connections(i, j) = 1;
            connections(j, i) = 1;
-       elseif row_sum == 0 && j == n
+       elseif row_sum == 0 && j == (i-1)
           connections(i, j) = 1;
           connections(j, i) = 1;
        end
