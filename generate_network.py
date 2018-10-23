651d65be945f6de23db2002395d780fd39554e86
simonsben <simons.ben0@gmail.com>
2018-10-23 15:01:44 -0400

    Refectored code

--- generate_network.py
+++ generate_network.py
@@ -1,12 +1,6 @@
 # Library imports
-import networkx as nx
-import matplotlib.pyplot as plt
+from utilities import generate_network, plot_network
 
 # Initialize graph
-n = 50
-G = nx.barabasi_albert_graph(n, 1)
-
-# Plot graph
-nx.draw(G)
-plt.draw()
-plt.show()
+G, w = generate_network(50)
+plot_network(G)
--- utilities.py
+++ utilities.py
@@ -1,6 +1,22 @@
+# Import libraries
 from random import random
+import networkx as nx
+import matplotlib.pyplot as plt
+
 
 def evaluate_probability(probability):
     return random() <= probability
 
-def generate_network
+
+# Generate new network
+def generate_network(n):
+    network = nx.barabasi_albert_graph(n, 1)
+    initial_weights = [.5] * n
+    return network, initial_weights
+
+
+# Plot network
+def plot_network(graph):
+    nx.draw(graph)
+    plt.draw()
+    plt.show()
