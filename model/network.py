6843994e2a1f67abc3cdab4adc8895eb98bbbfa5
simonsben <simons.ben0@gmail.com>
2018-11-20 15:38:16 -0500

    Re-wrote polya to use pre-allocated arrays instead (5 times faster)

--- model/network.py
+++ model/network.py
@@ -2,7 +2,7 @@ from typing import List
 from utilities.plot import plot_network
 from utilities.utilities import balls_per_node, balls_added, generate_plot_network
 from model.polya_node import polya_node
-from numpy import mean, sum
+from numpy import mean, zeros
 from utilities.io import save_network
 from random import random
 
@@ -16,7 +16,7 @@ class network:
         self.weights = []
         self.contagion = []
         self.exposures = []
-        self.node_exposures = []
+        self.node_exposures = zeros(n)
 
         self.n = n
         self.steps = 0
@@ -41,21 +41,31 @@ class network:
 
     def calculate_exposure(self):
         delta_balls = self.steps * balls_added
-        ball_counts = [[node.red, node.init_total + delta_balls]
-                       for node in self.nodes]
+        ball_counts = zeros((self.n, 2))
+        urn_counts = zeros((self.n, 2))
 
-        urn_counts = [sum([ball_counts[node.id],
-                           sum([ball_counts[neighbour.id] for neighbour in node], axis=0)
-                           ], axis=0) for node in self.nodes]
+        for i in range(self.n):
+            ball_counts[i, 0] = self.nodes[i].red
+            ball_counts[i, 1] = self.nodes[i].init_total + delta_balls
 
-        self.node_exposures = [nums[0] / nums[1] for nums in urn_counts]
+        for i in range(self.n):
+            urn_counts[i] = ball_counts[i]
+            for node in self.nodes[i]:
+                urn_counts[i, 0] += ball_counts[node.id, 0]
+                urn_counts[i, 1] += ball_counts[node.id, 1]
+
+            self.node_exposures[i] = urn_counts[i, 0] / urn_counts[i, 1]
+
         exposure = mean(self.node_exposures)
         self.exposures.append(exposure)
 
         return exposure
 
     def run_step(self):
-        picked_balls = [(0 if random() < exposure else 1) for exposure in self.node_exposures]
+        # picked_balls = [(0 if random() < exposure else 1) for exposure in self.node_exposures]
+        picked_balls = zeros(self.n)
+        for i in range(self.n):
+            picked_balls[i] = 0 if random() < self.node_exposures[i] else 1
 
         for i, node in enumerate(self.nodes):
             node.add_ball(picked_balls[i])
