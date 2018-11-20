4b647a285f0440be5ee5ef9fc05838a0cff983e7
simonsben <simons.ben0@gmail.com>
2018-11-20 14:45:49 -0500

    Save point, doing benchmark

--- tests/test_network.py
+++ tests/test_network.py
@@ -1,13 +1,20 @@
 from model.network import network
-from networkx import to_numpy_array
+from time import time_ns
+from numpy import mean, std
 
-net = network(15)
-print(to_numpy_array(net.network_plot))
+times = []
+for i in range(20):
+    net = network(1000)
+    print(net.exposures[len(net.exposures)-1])
 
-print(net)
-print(net.exposures)
+    start = time_ns()
+    net.run_n_steps(100)
+    end = time_ns()
 
-net.run_n_steps(10)
-print(net)
-print(net.exposures)
+    print('execute_time', (end - start) / 1000000000)
+    times.append((end - start) / 1000000000)
 
+print(mean(times), std(times))
+print(net.exposures[len(net.exposures)-1])
+
+# 2.572643455 0.06732472111681173
