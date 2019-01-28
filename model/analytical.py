8ec5c0117759c776da7d9fd70dc33bf8e38b20b2
simonsben <simons.ben0@gmail.com>
2019-01-28 10:45:22 -0500

    Fixed exposure function and added functions to generate common networks

--- model/analytical.py
+++ model/analytical.py
@@ -1,31 +1,62 @@
 from scipy.optimize import minimize
 from numpy import array, sum, round
-from math import fabs
+# from model.generator import wheel_graph
 
 # x = [R_1, R_2, R_3, B_1, B_2, B_3]
 #       0    1    2    3    4    5
-N = 10
+N = 12
 balls_per = 5
 num_balls = N * balls_per
 R = [balls_per] * N
 
-# Generate line network
-network = [[(1 if fabs(i-j) <= 1 else 0) for j in range(N)] for i in range(N)]
+# k=2, n=8
+# network = [
+#     [1, 1, 0, 1, 0, 1, 0, 0],
+#     [1, 1, 1, 0, 0, 0, 1, 0],
+#     [0, 1, 1, 1, 0, 0, 0, 1],
+#     [1, 0, 1, 0, 1, 0, 0, 0],
+#     [0, 0, 0, 1, 1, 1, 0, 1],
+#     [1, 0, 0, 0, 1, 1, 1, 0],
+#     [0, 1, 0, 0, 0, 1, 1, 1],
+#     [0, 0, 1, 0, 1, 0, 1, 1]
+# ]
+
+# k=3, n=12
+network = [
+    [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+    [0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0],
+    [0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1]
+]
+print(array(network))
+for row in network:
+    if sum(row) != 4:
+        print('Bad row!')
+        exit()
 
 
 # Define exposure function
 def exposure(B):
-
     expsr = 0
     for node in network:                        # For each node
         r_sum, b_sum = 0, 0
         for i, rel in enumerate(node):          # Sum all neighbours
-            if rel != 0:                        # Only sum if there is a connection
+            if rel != 0:            # Only sum if there is a connection
                 r_sum += R[i]
                 b_sum += B[i]
 
-        if r_sum + b_sum != 0:                  # Only add to sum if non-zero denominator
+        if (r_sum + b_sum) != 0:                  # Only add to sum if non-zero denominator.
             expsr += r_sum / (r_sum + b_sum)
+
+    # print(B, expsr)
 
     return expsr
 
@@ -44,7 +75,13 @@ cons = {'type': 'eq', 'fun': cons_func}
 ops = {'disp': True}
 
 # Define initial conditions
-init = array([balls_per] * N)
+# init = array([balls_per] * N)
+init = array([
+    # [0, 15, 0, 0, 15, 0]
+    # [20, 0, 0, 0, 0, 0, 0, 20]
+    [20, 0, 0, 0, 20, 0, 0, 0, 20, 0, 0, 0]
+    # [10, 0, 10, 0]
+])
 
 # Run optimization
 optimal = minimize(exposure, init, constraints=cons, method='SLSQP', options=ops, bounds=bounds)
---
+++ model/generator.py
@@ -0,0 +1,38 @@
+from math import fabs
+
+
+# Function to generate a line network
+def line_graph(N):
+    return [[(1 if fabs(i - j) <= 1 else 0) for j in range(N)] for i in range(N)]
+
+
+# Function to generate a circular network
+def circular_graph(N):
+    network = line_graph(N)
+    network[0][N-1] = 1
+    network[N-1][0] = 1
+
+    return network
+
+
+# Function to generate a star graph
+def star_graph(N):
+    network = [[(1 if j == i or j == 0 else 0) for j in range(N)] for i in range(N)]
+    for i, _ in enumerate(network[0]):
+        network[0][i] = 1
+
+    return network
+
+
+# Function to generate a wheel graph
+def wheel_graph(N):
+    network = star_graph(N)
+
+    for i, node in enumerate(network):
+        left = (i - 1) if (i-1) > 0 else N - 1
+        right = (i + 1) if (i+1) < N else 1
+
+        node[left] = 1
+        node[right] = 1
+
+    return network
