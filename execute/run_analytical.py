e4e666b3a90d74670ce52e828594653e58d01dfa
simonsben <simons.ben0@gmail.com>
2019-01-29 12:21:40 -0500

    Cleaned up a bit more (misc)

--- execute/run_analytical.py
+++ execute/run_analytical.py
@@ -1,25 +1,33 @@
 from model.analytical import optimize_distribution
 from model.generator import circular_graph, line_graph, wheel_graph
 from numpy import array, round
+from math import floor
 
 
-N = 4
+N = 7
 balls_per = 5
 num_balls = N * balls_per
+# R = [1] * N
+# R[0] = num_balls - (N-1)
 R = [0] * N
 R[0] = num_balls
-# R = [balls_per] * N
+
 B = [balls_per] * N
-rounds = 3
+rounds = 5
 
 network = line_graph(N)
 print(array(network))
 
+print(R, B)
+
 for i in range(rounds):
+    print('Round', i+1)
     optimal = optimize_distribution(network, num_balls, N, R, B)
     B = round(optimal.x)
-    print('Optimal ball placement:', R, B)
+    print('Black move:', 'R', R, 'B', B)
 
+
+    # TODO double check that optimization is flipped
     optimal = optimize_distribution(network, num_balls, N, B, R)
     R = round(optimal.x)
-    print('Optimal ball placement:', R, B)
+    print('Red move:', 'R', R, 'B', B)
