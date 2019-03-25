90d0d8fdc9076c4089458b2ec1ee48ad548c223b
simonsben <simons.ben0@gmail.com>
2019-03-25 10:56:20 -0400

    Ran analytical solver

--- execute/run_gradient.py
+++ execute/run_gradient.py
@@ -1,15 +1,11 @@
 from execute.import_data import load_airport_and_route
 from networkx import from_edgelist, number_of_nodes
 from model import network, optimize_distribution
-from utilities import save_distribution, balls_per_node, filter_degree, re_index
+from utilities import save_distribution, balls_per_node
 from time import time
 
-filter_level = 100
-
 # Import and filter data
-nodes, edges = load_airport_and_route(filter_data=False)
-nodes, _, edges = filter_degree(nodes, edges, d_cut_off=filter_level)
-nodes, edges = re_index(nodes, edges)
+nodes, edges = load_airport_and_route()
 
 # Generate network
 netx = from_edgelist(edges)
@@ -23,12 +19,13 @@ budget = balls_per_node * N
 
 # Optimize
 start = time()
-distribution, _ = optimize_distribution(neighbourhoods, R, B, budget)
+distribution, exp = optimize_distribution(neighbourhoods, R, B, budget)
 end = time()
 
 print('Execution time: ' + str(end-start) + ' s')
+print('Final exposure: ' + str(exp))
 
 
-file_name = '../data/optimal_distribution/f_' + str(filter_level) + '_analytical.csv'
+file_name = '../data/optimal_distribution/analytical.csv'
 save_distribution(distribution, alt_file_name=file_name)
 print(distribution)
