025442c7dad0507d85c957e289f7f980cf3b4532
simonsben <simons.ben0@gmail.com>
2019-03-31 12:17:45 -0400

    Ran analytical solver

--- execute/run_gradient.py
+++ execute/run_gradient.py
@@ -1,10 +1,12 @@
 from execute.import_data import load_airport_and_route
-from networkx import from_edgelist, number_of_nodes
+from networkx import from_edgelist, number_of_nodes, degree
 from model import network, optimize_distribution
 from utilities import save_distribution, balls_per_node
 from time import time
+from numpy import array
 
 # Import and filter data
+uniform = False
 nodes, edges = load_airport_and_route()
 
 # Generate network
@@ -17,6 +19,12 @@ neighbourhoods = [[neigh.id for neigh in node] for node in net.nodes]
 R, B = [balls_per_node] * N, [balls_per_node] * N
 budget = balls_per_node * N
 
+if not uniform:
+    degrees = degree(netx)
+    degrees = array(sorted(degrees, key=lambda d: d[1], reverse=True))
+    R = [0] * N
+    R[degrees[0, 0]] = N * balls_per_node
+
 # Optimize
 start = time()
 distribution, exp = optimize_distribution(neighbourhoods, R, B, budget)
@@ -26,6 +34,7 @@ print('Execution time: ' + str(end-start) + ' s')
 print('Final exposure: ' + str(exp))
 
 
-file_name = '../data/optimal_distribution/analytical.csv'
+base_path = '../data/optimal_distribution/'
+file_name = base_path + ('uniform_red.csv' if uniform else 'single_red.csv')
 save_distribution(distribution, alt_file_name=file_name)
 print(distribution)
